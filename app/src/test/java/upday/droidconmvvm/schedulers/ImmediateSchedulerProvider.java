d6c325717fd5e8624f82978d3179cf6c7b142315
Steve Madsen <steve@samknows.com>
2018-10-31 13:43:31 +0000

    Migrate to Rx2 schedulers

--- app/src/test/java/upday/droidconmvvm/schedulers/ImmediateSchedulerProvider.java
+++ app/src/test/java/upday/droidconmvvm/schedulers/ImmediateSchedulerProvider.java
@@ -3,6 +3,7 @@ package upday.droidconmvvm.schedulers;
 import android.support.annotation.NonNull;
 
 import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -13,12 +14,12 @@ public class ImmediateSchedulerProvider implements ISchedulerProvider {
     @NonNull
     @Override
     public Scheduler computation() {
-        return Schedulers.immediate();
+        return Schedulers.computation();
     }
 
     @NonNull
     @Override
     public Scheduler ui() {
-        return Schedulers.immediate();
+        return AndroidSchedulers.mainThread();
     }
 }
